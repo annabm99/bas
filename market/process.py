91d3e4f36d26f962ecaa7b9fc83babd2bfe4840c
annabm99 <anna.basquet10@gmail.com>
2023-02-22 19:38:43 +0100

    clustal working

--- market/process.py
+++ market/process.py
@@ -1,40 +1,34 @@
-
-clustal_input_file = "input.fasta"
-
 ### PROCESS PASTED SEQUENCES
-def process_sequences(sequence_string):
+def process_sequences(sequence_string, clustal_input_file = "input.fasta"):
     with open (clustal_input_file, "w") as fd:
-            print (sequences, file=fd)
+            print (sequence_string, file=fd)
     # Generates a file.fasta with the pasted sequences
 
 ### PROCESS PASTED UNIPROT IDs
-import requests as r
-from Bio import SeqIO
-from io import StringIO
 
-def process_uniprot(id_list):
+def process_uniprot(ids, clustal_input_file = "input.fasta"):
+    import requests as r
+    from Bio import SeqIO
+    from io import StringIO
+    id_list = ids.split(", ")
     baseUrl="http://www.uniprot.org/uniprot/"
     fd = open (clustal_input_file, "w")
+    
     for id in id_list:
+        print(id)
         currentUrl=baseUrl+id+".fasta"
         response = r.post(currentUrl) # request to uniprot
         cData=''.join(response.text)
         print(cData, file=fd)
     fd.close()
 
-id_list = ("P38398", "P51587")
-process_uniprot(id_list)
 
 ### EXECUTE CLUSTALO
-from Bio.Align.Applications import ClustalOmegaCommandline
-import os
 
-output_format = "fasta"
-
-# a2m=fa[sta],clu[stal],msf,phy[lip],selex,st[ockholm],vie[nna]} MSA output file format (default: fasta)
-
-def execute_clustalo(in_file):
-    clustalomega_cline = ClustalOmegaCommandline(infile = in_file, outfile = "outfile", outfmt = output_format, verbose = True, auto = False)
+def execute_clustalo(output_format, input_file = "input.fasta"):
+    from Bio.Align.Applications import ClustalOmegaCommandline
+    import os
+    clustalomega_cline = ClustalOmegaCommandline(infile = input_file, outfile = "outfile", outfmt = output_format, verbose = True, auto = False, force = True)
     o = str(clustalomega_cline)
     stream = os.popen(o)
     output = stream.read()
--- market/routes.py
+++ market/routes.py
@@ -1,10 +1,12 @@
+#! usr/bin/env python
+
 from market import app # Importing the app
 from flask import render_template, redirect, url_for, flash # Import package to read templates
 from market.models import Item, User # Import Item from the database
 from market.forms import SequenceForm # Import register form from the forms file
 from werkzeug.utils import secure_filename
 import os
-
+from market import process
 
 # HOMEPAGE
 @app.route("/") # Root url
@@ -23,19 +25,32 @@ def input_page():
     return render_template('input.html', item=name)
     # Whatever is put into name will be sent to the html jinja, and it will be displayed on the webpage
 
-
-@app.route("/market")
-def market_page():
-    items = Item.query.all() # Refers to the Item table from the database, which will be displayed on the page
-    return render_template("market.html", items=items)
-
-# CLUSTAL PAGE
 @app.route("/clustalo", methods=["GET", "POST"]) # This route can support get and post requests
 def clustal_app():
     form = SequenceForm() # Referring to register form in the forms.py file
-    if form.validate_on_submit:
+    # This will only be executed if the user clicks on the validate button from the form:
+    if form.validate_on_submit():
         sequences = form.sequences.data
-        ids = form.sequences.data
-
+        if sequences:
+            process.process_sequences(sequences)
+        id_list = form.ids.data
+        if id_list:
+            process.process_uniprot(id_list) 
+        clustal_input_file = form.inputfile.data
+        if clustal_input_file:
+            clustal_input_file.save(secure_filename("input.fasta"))       
+        output_format = form.outputformat.data
+        process.execute_clustalo(output_format[0])
+        return redirect(url_for("clustal_done"))
+    if form.errors != {}: # if form.errors is not empty, check for errors
+        for err_msg in form.errors.values(): # go through error messages
+            # Instead of printing to the terminal, flash prints to the user interface so users can be aware of errors
+            flash(f"There was an error in creating the user: {err_msg}")
     # Display fields of the form, through register.html file
     return render_template("clustalo.html", form=form)
+
+@app.route("/clustalo/output")
+def clustal_done():
+    with open ("outfile", "r") as f:
+        content = f.read()
+    return render_template('market.html', content=content)
