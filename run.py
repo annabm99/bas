cee94d2feca9a3fe0d92613da4f6d70adc98a9fa
annabm99 <anna.basquet10@gmail.com>
2023-02-23 17:16:42 +0100

    new name

--- run.py
+++ run.py
@@ -1,6 +1,4 @@
-from market import app # call app variable from the init file.
-    # market is the subdirectory where the app is saved
-
+from web import app # call app variable from the init file.
 
 if __name__=='__main__': # Command to run the app
     app.run(debug=True) # True so we can see edits real-time
