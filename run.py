5577c97e8d94ad641188414292487619c55038bb
annabm99 <anna.basquet10@gmail.com>
2023-02-28 10:23:44 +0100

    final web

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 from web import app # call app variable from the init file.
 
 if __name__=='__main__': # Command to run the app
-    app.run(debug=True) # True so we can see edits real-time
+    app.run(debug=False) # True so we can see edits real-time
