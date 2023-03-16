fd9b4935911483fa5a6d493b633ec91cc58f4f9d
Pau Andrio <andriopau@gmail.com>
2023-03-16 13:01:04 +0100

    Deploy settings

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from web import app # call app variable from the init file.
+from web import hostedApp as app # call app variable from the init file.
 
 if __name__=='__main__': # Command to run the app
     app.run(debug=False) # True so we can see edits real-time
--- web/__init__.py
+++ web/__init__.py
@@ -3,6 +3,8 @@
 
 from flask import Flask, render_template # Import flask instance from Flask package
 from flask_sqlalchemy import SQLAlchemy # Package needed to create the database
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from werkzeug.exceptions import NotFound
 
 # render_template is to address templates (html)
 app = Flask (__name__,
@@ -13,5 +15,15 @@ app = Flask (__name__,
 app.config['SECRET_KEY'] = '34d60585b853a486c59090bf'
 app.config['UPLOAD_FOLDER'] = './'
 app.config['ALLOWED_EXTENSIONS'] = set(['*.fa', '*.fasta'])
+
+hostedApp = Flask (__name__,
+            static_url_path='', 
+            static_folder='./static',
+            template_folder='./templates')
+
+hostedApp.config['SECRET_KEY'] = '34d60585b853a486c59090bf'
+hostedApp.config['UPLOAD_FOLDER'] = './'
+hostedApp.config['ALLOWED_EXTENSIONS'] = set(['*.fa', '*.fasta'])
+hostedApp.wsgi_app = DispatcherMiddleware(NotFound(), {f"/u194046":app})
 
 from web import routes # Import routes (from the routes file)
